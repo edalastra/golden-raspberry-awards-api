525e669b50ef97848e00dc634a19d12744bd81d1
Elizandro Gabriel Dalastra <edalastra@uolinc.com>
2026-03-06 12:54:28 -0300

    implementa trace id

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import router from '../shared/http/routes.js';
+import { putTraceIdInRequest } from '../shared/http/middlewares/index.js';
 
 export function createApp() {
   const app = express();
+  app.use(putTraceIdInRequest);
   app.use('/v1', router);
   app.use(bodyParser.json());
   app.use(cors());
