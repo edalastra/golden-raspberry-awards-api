15cdbea84ddc49084da2886c4dc818b63d01e216
Elizandro Gabriel Dalastra <edalastra@uolinc.com>
2026-03-07 14:37:49 -0300

    implementa swagger

--- src/config/app.ts
+++ src/config/app.ts
@@ -4,11 +4,13 @@ import bodyParser from "body-parser";
 import router from "../shared/http/routes.js";
 import { putTraceIdInRequest } from "../shared/http/middlewares/index.js";
 import { errorMiddleware } from "../shared/http/middlewares/app-error.middleware.js";
+import { swaggerUi, swaggerDocument } from "./swagger.js";
 
 export function createApp() {
   const app = express();
   app.use(putTraceIdInRequest);
   app.use("/v1", router);
+  app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use(bodyParser.json());
   app.use(cors());
   app.use(errorMiddleware);
